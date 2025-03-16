19a26721e7afe5edb0a20cf1a4a63d9b11b5ef16
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-03-16 16:44:42 -0500

    Ejercicios Variables y Tipos de Datos Python

---
+++ Ejercicios Python/T1_variables/T1_variables.py
@@ -0,0 +1,32 @@
+# VARIABLES EN PYTHON
+
+miVariable = "Hola Pyhton"
+print(miVariable)
+
+# Ejemplos de asignaciones
+
+nombre  = "Juan"
+edad = 20
+altura = 1.70
+esEstudiante = True
+
+print(nombre)
+print(edad)
+print(altura)
+print(esEstudiante)
+
+# Valores a las variables
+
+x = 10
+y = 20
+
+z = x + y
+print(z)
+
+
+# Concatenacion 
+
+usuario = "User1"
+mensajeBienveida = "Hola, " + usuario
+
+print(mensajeBienveida)
---
+++ Ejercicios Python/T1_variables/T2_tiposDatos.py
@@ -0,0 +1,11 @@
+# TIPOS DE DATOS EN PYTHON
+
+entero = 10 # int
+cadenaTexto = "Texto cualquiera" # str
+flotante = 10.20 # float
+booleano = True # boolean
+
+print(type(flotante))
+print(type(entero))
+print(type(booleano))
+print(type(cadenaTexto))
